83a9ab7998819de1b96d094108c0225779304340
Alexandru Dirig <dirig.alexandru@yahoo.com>
2019-08-16 12:45:13 +0300

    Test Cases

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -6,27 +6,46 @@ import java.util.ArrayList;
 
 public class Tests {
 
-  ArrayList<ContBancar> contAlex = new ArrayList<ContBancar>();
+    ArrayList<ContBancar> contAlex = new ArrayList<ContBancar>();
 
 
-  @Test
-  public void Test1Salariu () {
+    @Test
+    public void Test1Salariu() {
 
-    contAlex.add(new Depunere(1, "Salariu luna mai", "20190505", 10));
+        contAlex.add(new Depunere(1, "Salariu luna mai", "20190505", 10));
+        InterogareSold interogare = new InterogareSold(contAlex);
+        System.out.println(interogare.getBalance());//
+        Assert.assertEquals(10, interogare.getBalance());
+    }
 
-    InterogareSold interogare = new InterogareSold(contAlex);
-    System.out.println(interogare.getBalance());//
-    Assert.assertEquals(10, interogare.getBalance());
-  }
+    @Test
+    public void Test2Retragere() {
+        contAlex.add(new Depunere(2, "Salariu luna mai", "20190801", 500));
+        contAlex.add(new Retragere(3, "cumparaturi la piata", "20190816", 20, "Groceries"));
+        InterogareSold interogare = new InterogareSold(contAlex);
+        System.out.println(interogare.getBalance());
+        Assert.assertEquals(480, interogare.getBalance());
+    }
 
+    @Test
+    public void Test3PrintDepunere() {
+        String data = new String();
+        double suma = 0;
 
-  @Test
-  public void Test2Demo () {
+        contAlex.add(new Depunere(2, "Salariu luna mai", "20190801", 500));
+        System.out.println(contAlex.toString());
 
-    contAlex.add(new Depunere(2, "Salariu luna mai", "20190505", 15));
+        for (ContBancar item : contAlex) {
+            data = item.data;
+            suma = item.suma;
+        }
+        Assert.assertEquals("[Depunere [data=" + data + ", suma=" + suma + "]]", contAlex.toString());
+    }
 
-    InterogareSold interogare = new InterogareSold(contAlex);
-    System.out.println(interogare.getBalance());//
-    Assert.assertEquals(15, interogare.getBalance());
-  }
+    @Test
+    public void Test4PrintRetragere() {
+        contAlex.add(new Retragere(3, "cumparaturi la piata", "20190816", 20, "Groceries"));
+        System.out.println(contAlex.toString());
+        Assert.assertEquals("["+contAlex.get(0)+"]", contAlex.toString());
+    }
 }
